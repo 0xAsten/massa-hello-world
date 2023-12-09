84abb7703a7805fe1cf4b48341f07fbaec508336
Asten <>
2023-12-09 20:47:36 +0800

    app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Client,
+  ClientFactory,
+  strToBytes,
+  bytesToStr,
+  WalletClient,
+  DefaultProviderUrls,
+} from '@massalabs/massa-web3';
 
-function App() {
+import { useEffect, useState } from 'react';
+
+const sc_addr = 'AS12DNtXixaMWbDZRM2ECQrXnJLgr8hmhFdsM1HNzu4mi3TsSfHKm';
+
+/**
+ * Content component that handles interactions with a Massa smart contract
+ * @returns The rendered component
+ */
+function Content() {
+  /**
+   * State variables to manage the web3 client and greetings
+   */
+  const [web3client, setWeb3client] = useState<Client | null>(null);
+  const [greeting, setGreeting] = useState<string | null>(null);
+
+  /**
+   * Initialize the web3 client using the custom provider defined above
+   */
+  useEffect(() => {
+    const initClient = async () => {
+      const client = await ClientFactory.createDefaultClient(
+        DefaultProviderUrls.BUILDNET,
+        false,
+      );
+      setWeb3client(client);
+    };
+    initClient().catch(console.error);
+  }, []);
+
+  /**
+   * Fetch the greeting when the web3 client is initialized
+   */
+  useEffect(() => {
+    if (web3client) {
+      funcGetGreeting();
+    }
+  }, [web3client]);
+
+  /**
+   * Function to get the current greeting from the smart contract
+   */
+  async function funcGetGreeting() {
+    if (web3client) {
+      let res = await web3client
+        .publicApi()
+        .getDatastoreEntries([
+          { address: sc_addr, key: strToBytes('greeting') },
+        ]);
+      if (res[0].candidate_value) {
+        let greetingDecoded = bytesToStr(res[0].candidate_value);
+        setGreeting(greetingDecoded);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div>Greeting: {greeting}</div>
     </div>
   );
 }
 
-export default App;
+export default Content;
